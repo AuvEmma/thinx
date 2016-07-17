3a27098796fc62de6341aa55257f6444d33eb682
Emma Hou <emma.houy@gmail.com>
2016-07-17 19:55:41 -0400

    set quantity

--- js/script.js
+++ js/script.js
@@ -2,4 +2,22 @@
 $(document).ready( function(){
   console.log('color.js has loaded');
 
+  $('.input-number').click(function(e){
+    e.preventDefault();
+    var num         = $('#quantity-num');
+    var type        = $(this).attr('data-type');
+    var currentVal  = parseInt(num.val());
+    if (type === 'plus') {
+      currentVal++;
+    }else {
+      if (currentVal <= 0) {
+        $('#minus')[0].setAttribute('disabled','disabled')
+      }else{
+        currentVal--;
+      }
+    }
+    console.log(currentVal);
+    $('#quantity-num').val(currentVal)
+    $('#quantity').val('quantity(' + currentVal + ')');
+  });
 })
